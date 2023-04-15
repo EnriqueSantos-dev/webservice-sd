4590ea78cf9428db094f40af7d2cd73e92dc6eeb
Enrique Santos <santosenrique869@gmail.com>
2023-04-15 15:43:50 -0300

    chore: add .editor.config file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,27 +3,34 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./global.css";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
 } from "react-router-dom";
 import { Step1 } from "./components/forms/Step1";
 import { Step2 } from "./components/forms/Step2";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<App />} path="/">
-      <Route element={<Step1 />} index />
-			<Route path="/step1" element={<Step1 />}/>
-      <Route path="/step2" element={<Step2 />} />
-			<Route path="*" element={<div className="text-red-500 text-4xl text-center">route not found</div>} />
-    </Route>
-  )
+	createRoutesFromElements(
+		<Route element={<App />} path="/">
+			<Route element={<Step1 />} index />
+			<Route path="/step1" element={<Step1 />} />
+			<Route path="/step2" element={<Step2 />} />
+			<Route
+				path="*"
+				element={
+					<div className="text-red-500 text-4xl text-center">
+						route not found
+					</div>
+				}
+			/>
+		</Route>
+	)
 );
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
