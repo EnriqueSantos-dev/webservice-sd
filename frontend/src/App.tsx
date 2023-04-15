f9081962b83a1283e63e6070ef6d15b86f8f3f82
Enrique Santos <santosenrique869@gmail.com>
2023-04-15 15:43:00 -0300

    refactor: add header element in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,9 @@ import { Toaster } from "react-hot-toast";
 export function App() {
   return (
     <FormContextProvider>
+			<header className="h-20 flex justify-center items-center">
+				<h2 className="text-2xl text-white font-bold">Aluguel de Carros</h2>
+			</header>
       <Outlet />
       <Toaster position="top-right" />
     </FormContextProvider>
